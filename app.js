1caa82198c5d4f5e2939b1a9f26fe8d44696f8ec
cantrellnm <natalie.m.cantrell@gmail.com>
2020-07-10 22:32:27 -0700

    Change env var used for db url

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(httpsRedirect);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useNewUrlParser', true);
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.DB_URI);
 mongoose.connection.on('error', (err) => {
   console.error(err);
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
